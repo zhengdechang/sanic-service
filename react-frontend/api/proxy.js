b936edf63c1b99bd645425cac4d4bc26c1cee6aa
zhengdechang <2473023641@qq.com>
2023-09-05 22:11:10 +0800

    update proxy.js

--- react-frontend/api/proxy.js
+++ react-frontend/api/proxy.js
@@ -6,13 +6,15 @@ module.exports = (req, res) => {
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     if (req.url.startsWith('/api')) {
-        target = 'https://866ec7b2-8000-app.lightly.teamcode.com'
+        target = 'https://866ec7b2-8000-app.lightly.teamcode.com/'
     }
 
     // 创建代理对象并转发请求
     createProxyMiddleware({
         target,
         changeOrigin: true,
+        pathRewrite: {
+            '^/api': ''  // 将请求路径重写为空，以便代理到目标地址的根路径
+        }
     })(req, res)
 }
-
